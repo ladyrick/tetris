3b65480922b23ce909ce5596355fae1b5da54891
liuhong12th <liuhong12th@163.com>
2017-03-23 01:34:29 +0800

    修改了一堆

--- tetris.js
+++ tetris.js
@@ -9,15 +9,26 @@ function getClassName(x, y) {
 }
 (function () {
     /*构造一个style节点。屏幕左上角为坐标(0,0)，横向变x，纵向变y。*/
-    var style = "";
-    for (var i = 0; i < w; i++) {
-        for (var j = 0; j <= h; j++) {
+    var style = "#container div{";
+    style += "position:absolute;";
+    style += "transition:0.2s;}\n";
+    for (var j = -4; j <= h; j++) {
+        var color = "rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + ")";
+        for (var i = 0; i < w; i++) {
             style += "." + getClassName(i, j) + "{";
-            style += "position:absolute;"
-            style += "background-color:black;";
-            style += "transition:0.2s;"
-            style += "width:" + (wPixels / w - 4) + "px;height:" + (j === h ? 0 : (hPixels / h - 4)) + "px;";
-            style += "margin-left:" + (wPixels / w * i + 2) + "px;margin-top:" + (hPixels / h * j + 2) + "px;}\n";
+            style += "background-color:" + color + ";";
+            style += "width:" + (wPixels / w - 4) + "px;";
+            if (j === h) {//touch the bottom
+                style += "height:" + 0 + "px;";
+                style += "margin-left:" + (wPixels / w * i + 2) + "px;margin-top:" + (hPixels + 2) + "px;}\n"
+            } else if (j < 0) {//higher than the roof
+                style += "height:" + 0 + "px;";
+                style += "margin-left:" + (wPixels / w * i + 2) + "px;margin-top:" + 0 + "px;}\n"
+            } else {
+                style += "height:" + (hPixels / h - 4) + "px;";
+                style += "margin-left:" + (wPixels / w * i + 2) + "px;margin-top:" + (hPixels / h * j + 2) + "px;}\n";
+            }
+
         }
     }
 
@@ -41,26 +52,30 @@ function extend(Child, Parent) {
 
 
 
-function Piece(position, shape) {
-    var pose = 0;
+function Piece(position, shape, pose) {
+    this.initState();
     Piece.prototype.getPosition = function () {
         return position;
     }
-    Piece.prototype.setPosition = function (newpos) {
-        if (newpos[0] < 0) {
-            position[0] = 0;
+    Piece.prototype.setPosition = function (newPos) {
+        if (newPos[0] < 0) {
+            newPos[0] = 0;
         } else {
             shape[pose].forEach(function (x) {
-                if (newpos[0] + x[0] >= w) {
-                    newpos[0] -= newpos[0] + x[0] - w + 1;
+                if (newPos[0] + x[0] >= w) {
+                    newPos[0] -= newPos[0] + x[0] - w + 1;
                 }
             });
-            position[0] = newpos[0];
         }
-        if (newpos[1] < -1) {
-            position[1] = -1;
+        if (newPos[1] < -1) {
+            newPos[1] = -1;
+        }
+        if (this.canPlace(newPos)) {
+            position[0] = newPos[0];
+            position[1] = newPos[1];
+            return true;
         } else {
-            position[1] = newpos[1];
+            return false;
         }
     }
     Piece.prototype.getShape = function () {
@@ -79,10 +94,28 @@ function Piece(position, shape) {
                 break;
             default:
         }
-        this.setPosition(position);
-        return pose;
+        if (this.canPlace()) {
+            return pose;
+        } else {
+            return false;
+        }
+
     }
-    this.setPosition(position);
+    if (this.setPosition(position)) {
+        this.updatePiece();
+    }
+}
+Piece.prototype.state = [];
+Piece.prototype.initState = function () {
+    if (this.state.length === 0) {
+        for (var i = 0; i < w; i++) {
+            var col = [];
+            for (var j = 0; j < h; j++) {
+                col.push(0);
+            }
+            this.state.push(col);
+        }
+    }
 }
 Piece.prototype.getAllPosition = function () {
     var positions = [];
@@ -92,12 +125,20 @@ Piece.prototype.getAllPosition = function () {
     });
     return positions;
 }
-Piece.prototype.removePiece = function () {
-    if (this.divs !== undefined) {
-        this.divs.forEach(function (x) {
-            container.removeChild(x);
-        })
+Piece.prototype.canPlace = function (curPos) {
+    if (curPos === undefined) {
+        curPos = this.getPosition();
     }
+    var ps = [];
+    this.getShape()[this.getPose()].forEach(function (p) {
+        ps.push([curPos[0] + p[0], curPos[1] + p[1]]);
+    });
+    for (var i in ps) {
+        if (this.state[ps[i][0]][ps[i][1]] !== 0) {
+            return false;
+        }
+    }
+    return true;
 }
 Piece.prototype.updatePiece = function () {
     var positions = this.getAllPosition();
@@ -117,48 +158,88 @@ Piece.prototype.updatePiece = function () {
     }
 }
 Piece.prototype.turnLeft = function () {
-    this.changePose(-1);
-    this.updatePiece();
+    if (this.changePose(-1)) {
+        this.updatePiece();
+        return true;
+    } else {
+        return false;
+    }
 }
 Piece.prototype.turnRight = function () {
-    this.changePose(1);
-    this.updatePiece();
+    if (this.changePose(1)) {
+        this.updatePiece();
+        return true;
+    } else {
+        return false;
+    }
 }
 Piece.prototype.moveDown = function () {
     var curPos = this.getPosition();
-    this.setPosition([curPos[0], curPos[1] + 1]);
-    this.updatePiece();
+    if (this.setPosition([curPos[0], curPos[1] + 1])) {
+        this.updatePiece();
+        return true;
+    } else {
+        var _this = this;
+        this.getAllPosition().forEach(function (p) {
+            _this.state[p[0]][p[1]] = 1;
+        });
+        return false;
+    }
 }
 Piece.prototype.moveLeft = function () {
     var curPos = this.getPosition();
-    this.setPosition([curPos[0] - 1, curPos[1]]);
-    this.updatePiece();
+    if (this.setPosition([curPos[0] - 1, curPos[1]])) {
+        this.updatePiece();
+        return true;
+    } else {
+        return false;
+    }
 }
 Piece.prototype.moveRight = function () {
     var curPos = this.getPosition();
-    this.setPosition([curPos[0] + 1, curPos[1]]);
-    this.updatePiece();
+    if (this.setPosition([curPos[0] + 1, curPos[1]])) {
+        this.updatePiece();
+        return true;
+    } else {
+        return false;
+    }
 }
 
 
 
 
-function PicecLine(pos) {
+function PieceLine(position,pose) {
     var shape = [
         [[0, 0], [1, 0], [2, 0], [3, 0]],
         [[0, 0], [0, -1], [0, -2], [0, -3]]
     ];
-    Piece.call(this, pos, shape);
+    Piece.call(this, position, shape,pose);
 }
-extend(PicecLine, Piece);
+extend(PieceLine, Piece);
 
-
-
-var p1 = new PicecLine([100, 5]);
-p1.updatePiece();
-setTimeout(function () {
-    p1.turnLeft();
-    setInterval(function () {
-        p1.moveDown();
-    }, 1000);
-}, 1000);
+var onePiece;
+document.onkeydown = function (e) {
+    var keycode = e.which;
+    switch (keycode) {
+        case 38://up
+            onePiece.turnLeft();
+            break;
+        case 37://left
+            onePiece.moveLeft();
+            break;
+        case 39://right
+            onePiece.moveRight();
+            break;
+        case 40://down
+            //TODO
+            console.warn("down key not implemented.");
+            break;
+        default:
+    }
+}
+function main() {
+    if (onePiece === undefined || !onePiece.moveDown()) {
+        onePiece = new PieceLine([0, -1],0);
+    }
+}
+setInterval(main, 300);
